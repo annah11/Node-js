7734a3229acf9d65217c624d63c58231cadfcd3b
annah11 <hanamesfin67@gmail.com>
2024-10-18 10:03:12 +0300

    test

---
+++ learning/event/event.js
@@ -0,0 +1,14 @@
+import { EventEmitter } from 'eventemitter';
+// var events = require('events');
+var eventEmitter = new events.EventEmitter();
+
+//Create an event handler:
+var myEventHandler = function () {
+  console.log('I hear a scream!');
+}
+
+//Assign the event handler to an event:
+eventEmitter.on('scream', myEventHandler);
+
+//Fire the 'scream' event:
+eventEmitter.emit('scream');
